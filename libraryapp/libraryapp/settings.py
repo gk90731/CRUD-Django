449311e0cc755f338af80280e79e2a54b2af58d5
gk90731 <gk90731@gmail.com>
2019-12-15 19:12:24 +0530

    first

--- libraryapp/libraryapp/settings.py
+++ libraryapp/libraryapp/settings.py
@@ -74,12 +74,18 @@ WSGI_APPLICATION = 'libraryapp.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+'default': {
+'ENGINE': 'django.db.backends.mysql',
+'NAME': 'justexp',
+'USER': 'root',
+'PASSWORD': "",
+'HOST': "",
+'PORT': "",
+'OPTIONS': {
+'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
 }
-
+}
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
